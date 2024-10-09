8f60dd9850e79ec9eefcb03332505a89be73f55e
alexyeti <alexeymalykh2602@gmail.com>
2024-10-09 09:12:08 +0700

    1 commit
    test commit

---
+++ 3lessons.py
@@ -0,0 +1,22 @@
+#lst = [1.35,8,'hi']
+#f = 34
+#print(lst [0], lst[2])  списки
+#print (lst[1:2:]) срез
+#lst.append(f)
+#lst.insert(1,99)
+#print(lst)
+#dict_1 = {"you":14, "edas":12} you ключ, 14 значение, индекс you+14
+#london = {
+ #   'id': 1,
+  #  'name': 'London',
+   # 'it_vlan': 320,
+    #'user_vlan': 1010,
+    #'mngmt_vlan': 99,
+   # 'to_name': None,
+  #  'to_id': None,
+ #   'port': 'G1/0/11'
+#}
+#tuple_1 = ("mos",2,3,1,2,3,2,2,1,3,2,1,3,3,3,2,5,3,1)
+#print(tuple_1.count(2))
+set_1={1,2,3,4,5}
+print(set('HLLLLO'))
