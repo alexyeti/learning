f6cd46758fbc8bf8b1d93d69fde3401eb613e2a8
alexyeti <alexeymalykh2602@gmail.com>
2024-10-09 09:55:52 +0700

    functions
    functions addition, multiplication, difference, 
    division, force.

---
+++ 4lesson.py
@@ -0,0 +1,25 @@
+def force_num(num,force):
+    print(num**force)
+
+force_num(2,10)
+
+def multiplication_num(num_1,multiplication):
+    print(num_1*multiplication)
+
+multiplication_num(3,10)
+
+def division_num(num_1,division):
+    print(num_1/division)
+
+division_num(15,6)
+
+def difference_num(num_1,difference):
+    print(num_1-difference)
+
+difference_num(18,7)
+
+def addition_num(num_1,addition):
+    print(num_1+addition)
+
+addition_num(3,5)
+
